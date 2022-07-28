e9b63f9fe262326e281df0a63ee7dafc95626483
Osman Ordu <orduosmann@gmail.com>
2022-07-28 05:04:17 +0300

    Update script.js
    Code developed,
    Animation added,
    Counter added.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+const counters = document.querySelectorAll('.counter')
 const url = "https://api.apilayer.com/exchangerates_data/latest?base=TRY";
 
 const myHeaders = new Headers();
-myHeaders.append("apikey", "WjIXefZ2GRLg9PrlSeagJLyYBunNoTFp");
 
+myHeaders.append("apikey", "WjIXefZ2GRLg9PrlSeagJLyYBunNoTFp");
 
 
 const requestOptions = {
@@ -11,34 +12,73 @@ const requestOptions = {
   headers: myHeaders
 };
 
-// -- Async function
+// -- Async function 
 async function getData() {
-  try{
-   const response = await fetch(url, requestOptions);
-   const data = await response.json();
-  
-   const TRY = data.rates.TRY;
-   const USD = TRY / data.rates.USD;
-   const EUR = TRY / data.rates.EUR;
-   const GBP = TRY / data.rates.GBP;
+  try {
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
 
-    setData(USD,EUR,GBP);
-  } catch(error) {
-    console.warn(error)
-  };
+    const 
+    TRY = data.rates.TRY,
+    USD = TRY / data.rates.USD,
+    EUR = TRY / data.rates.EUR,
+    GBP = TRY / data.rates.GBP;
 
+
+  setData(USD,EUR,GBP)
+  } catch(err) {console.warn(err)}; 
 }
+getData();
 
 // -- Callback function
 function setData(USD,EUR,GBP){
 
-    const dollar = document.getElementById('dollar');
-    const euro = document.getElementById('euro');
-    const sterlin = document.getElementById('sterlin');
+    const 
+     dollar = document.getElementById('dollar'),
+     euro = document.getElementById('euro'),
+     sterlin = document.getElementById('sterlin'); 
 
-    dollar.innerText = USD.toFixed(3);
-    euro.innerText = EUR.toFixed(3);
-    sterlin.innerText = GBP.toFixed(3);
+    const
+     parsedUSD = USD.toFixed(3),
+     parsedEUR = EUR.toFixed(3),
+     parsedGBP = GBP.toFixed(3);
+
+    dollar.innerText = parsedUSD;
+    euro.innerText = parsedEUR;
+    sterlin.innerText = parsedGBP;
+        
+    dollar.setAttribute(`data-target`,`${parsedUSD}`);
+    euro.setAttribute(`data-target`,`${parsedEUR}`);
+    sterlin.setAttribute(`data-target`,`${parsedGBP}`)
+
+    counters.forEach(counter => {
+    counter.innerText = '0';
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const c = +counter.innerText ;
+        const increment = target / 50;
+        if(c < target ) { 
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 10)
+        } else {
+            counter.innerText = target
+        }
+    }
+  updateCounter();
+})
 }
 
-getData();
+// -- Js animation
+window.sr = new ScrollReveal();
+
+sr.reveal('.card',{
+  opacity:0,
+  scale:0.8,
+  duration:2000
+})
+
+sr.reveal('h2',{
+  opacity:0,
+  scale:0.8,
+  duration:2000
+})
